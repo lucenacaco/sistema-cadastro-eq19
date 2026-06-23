3f30b859c5b7f55736975eab1a8e87c81a834b7d
LucenaCaco <seu_email_do_github@exemplo.com>
2026-06-22 22:43:08 -0300

    fix: atualiza dependencias no pom e configura o Main.java

--- src/main/java/com/equipe19/Main.java
+++ src/main/java/com/equipe19/Main.java
@@ -9,16 +9,16 @@ import java.util.Map;
 public class Main {
     public static void main(String[] args) {
 
-        // Configuração do Banco de Dados usando Variáveis de Ambiente
+
         HikariConfig cfg = new HikariConfig();
         cfg.setJdbcUrl(System.getenv("DB_URL"));
         cfg.setUsername(System.getenv("DB_USER"));
         cfg.setPassword(System.getenv("DB_PASSWORD"));
-        cfg.setMaximumPoolSize(5); // Obrigatório limite de 5 conexões
+        cfg.setMaximumPoolSize(5);
 
         Javalin app = Javalin.create();
 
-        // Rota de teste obrigatória exigida pelo portal
+
         app.get("/ping", ctx -> {
             ctx.json(Map.of(
                     "status", "ok",
@@ -27,10 +27,10 @@ public class Main {
             ));
         });
 
-        // Rota inicial padrão
+
         app.get("/", ctx -> ctx.result("Sistema de Cadastramento - Equipe 19"));
 
-        // Iniciar obrigatoriamente na porta 8080
+
         app.start(8080);
     }
 }
